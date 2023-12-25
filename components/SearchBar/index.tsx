030f8f2591eb42662237dca4741dbfe95beae57e
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-12-25 21:53:27 +0530

    updated searchbar to redirect to google.com

---
+++ components/SearchBar/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { Search } from 'react-feather'
+
+function SearchBar() {
+    const [input, setInput] = useState('')
+    const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault()
+        if(input!== ""){
+            window.location.href = `https://www.google.com/search?q=${input}`
+        }
+    }
+    
+    return (
+        <form
+            onSubmit={onSubmit}
+            className="w-full flex items-center text-2xl border-1 shadow-accent shadow rounded-lg overflow-hidden"
+        >
+            <input
+                value={input}
+                onChange={(evt) => setInput(evt.target.value)}
+                type="text"
+                className="flex-1 p-4 border-none oultine-none bg-transparent focus:outline-none focus:border-none focus:ring-0"
+            />
+            <button type="submit" className="p-4">
+                <Search />
+            </button>
+        </form>
+    )
+}
+
+export default SearchBar
